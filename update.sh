872aec8ad7be3183a003a93375390cbf05a621ca
ecs0423 <35982261+ecs0423@users.noreply.github.com>
2018-02-07 09:31:05 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -10,8 +10,9 @@ iptables -I INPUT -p tcp --dport 22 -j DROP;iptables -I INPUT -s 118.163.94.0/8 
 iptables -I INPUT -p tcp --dport 8080 -j DROP;iptables -I INPUT -s 125.227.58.0/8 -p tcp --dport 8080 -j ACCEPT
 iptables -I INPUT -p tcp --dport 502 -j DROP;iptables -I INPUT -s 125.227.58.0/8 -p tcp --dport 502 -j ACCEPT
 iptables -I INPUT -p tcp --dport 503 -j DROP;iptables -I INPUT -s 125.227.58.0/8 -p tcp --dport 503 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
 echo '#!/bin/bash' > /etc/network/if-pre-up.d/iptables
-iptables-save >> /etc/iptables.rules
+echo '/sbin/iptables-restore < /etc/iptables.up.rules' >> /etc/network/if-pre-up.d/iptables
 chmod +x /etc/network/if-pre-up.d/iptables
 
 # get replace file
