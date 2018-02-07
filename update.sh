dfa2c393855aabd6baf26d9743ebf349667940e3
ecs0423 <35982261+ecs0423@users.noreply.github.com>
2018-02-07 21:25:18 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -11,16 +11,16 @@ mv nginx_passwd /etc/nginx/
 mv restart_app.sh /opt/gateway;mv rc.local /etc;rm -f restart_app.tgz
 
 # setup firewall
-# iptables -I INPUT -p tcp --dport 443 -j DROP;iptables -I INPUT -s 220.130.143.211 -p tcp --dport 443 -j ACCEPT
-# iptables -I INPUT -p tcp --dport 22 -j DROP;iptables -I INPUT -s 220.130.143.211 -p tcp --dport 22 -j ACCEPT
-# iptables -I INPUT -p tcp --dport 443 -j DROP;iptables -I INPUT -s 118.163.94.193 -p tcp --dport 443 -j ACCEPT
-# iptables -I INPUT -p tcp --dport 22 -j DROP;iptables -I INPUT -s 118.163.94.193 -p tcp --dport 22 -j ACCEPT
-# iptables -I INPUT -p tcp --dport 8080 -j DROP;iptables -I INPUT -s 125.227.58.74 -p tcp --dport 8080 -j ACCEPT
-# iptables -I INPUT -p tcp --dport 502 -j DROP;iptables -I INPUT -s 125.227.58.74 -p tcp --dport 502 -j ACCEPT
-# iptables -I INPUT -p tcp --dport 503 -j DROP;iptables -I INPUT -s 125.227.58.74 -p tcp --dport 503 -j ACCEPT
-# iptables-save > /etc/iptables.up.rules
-# sed 's/exit 0//g' -i /etc/rc.local
-# echo 'iptables-restore < \/etc\/iptables.up.rules' >> /etc/rc.local
+iptables -I INPUT -p tcp --dport 443 -j DROP;iptables -I INPUT -s 220.130.143.211 -p tcp --dport 443 -j ACCEPT;iptables -I INPUT -s 118.163.94.193 -p tcp --dport 443 -j ACCEPT
+iptables -I INPUT -p tcp --dport 22 -j DROP;iptables -I INPUT -s 220.130.143.211 -p tcp --dport 22 -j ACCEPT;iptables -I INPUT -s 118.163.94.193 -p tcp --dport 22 -j ACCEPT
+iptables -I INPUT -p tcp --dport 8080 -j DROP;iptables -I INPUT -s 125.227.58.74 -p tcp --dport 8080 -j ACCEPT
+iptables -I INPUT -p tcp --dport 502 -j DROP;iptables -I INPUT -s 125.227.58.74 -p tcp --dport 502 -j ACCEPT
+iptables -I INPUT -p tcp --dport 503 -j DROP;iptables -I INPUT -s 125.227.58.74 -p tcp --dport 503 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+sed 's/exit 0//g' -i /etc/rc.local
+echo 'sleep 10' >> /etc/rc.local
+echo 'iptables-restore < /etc/iptables.up.rules' >> /etc/rc.local
+echo 'exit 0' >> /etc/rc.local
 
 rm -f update.sh
 
